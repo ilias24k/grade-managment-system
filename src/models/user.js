de26b90a0bc9fbb13e488ada711cae6210438c43
ilias24k <ilias_24K@hotmail.com>
2023-08-30 11:48:09 +0300

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -70,8 +70,10 @@ userSchema.statics.findByCredentials = async (email, password) => {
 userSchema.methods.generateAuthToken = async function () {
     const user = this
     const token = jwt.sign({ _id: user._id.toString() }, 'grademanagment',{ expiresIn: '30d' }) // token exp date
+    //delete previous token
+    user.tokens = []
 
-    user.tokens = user.tokens.concat({ token })
+    user.tokens.push({ token })
     await user.save()
     return token
 }
